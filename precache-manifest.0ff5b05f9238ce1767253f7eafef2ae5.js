52f82ac009fbe9ce4b3e28f8030b3eab8bcc7e56
unknown <buffshloy55@gmail.com>
2020-07-22 18:11:59 +0200

    Updates

---
+++ precache-manifest.0ff5b05f9238ce1767253f7eafef2ae5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839f490426d86fb648926c0c66ee0894",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "308a9a9592047c142939",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ae13540e3e378aeedd4",
+    "url": "/portfolio/static/css/main.27a6d156.chunk.css"
+  },
+  {
+    "revision": "308a9a9592047c142939",
+    "url": "/portfolio/static/js/2.df292b1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.df292b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae13540e3e378aeedd4",
+    "url": "/portfolio/static/js/main.12d2238c.chunk.js"
+  },
+  {
+    "revision": "f48a01292bbc41da2323",
+    "url": "/portfolio/static/js/runtime-main.3425909f.js"
+  },
+  {
+    "revision": "42352dcbd2071aff8d8fba61c64fa7e8",
+    "url": "/portfolio/static/media/corona.42352dcb.jpeg"
+  },
+  {
+    "revision": "8d7ae5ac6e934401e57e82f70b996bc1",
+    "url": "/portfolio/static/media/geek.8d7ae5ac.png"
+  },
+  {
+    "revision": "9e0823c638311733b26f72e27e201be1",
+    "url": "/portfolio/static/media/insta.9e0823c6.jpg"
+  },
+  {
+    "revision": "63d92914c5984e020105bb6ece87c0e2",
+    "url": "/portfolio/static/media/porto.63d92914.jpeg"
+  },
+  {
+    "revision": "8d0f96c99af080cd54d6a1c2b37a7654",
+    "url": "/portfolio/static/media/smartbrain.8d0f96c9.png"
+  },
+  {
+    "revision": "eefcd58ad83fc9c19f3f951ada5cda07",
+    "url": "/portfolio/static/media/tanails.eefcd58a.jpg"
+  }
+]);
